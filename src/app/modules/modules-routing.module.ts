62e7738afd77b69aba5c340922ecdf9038ed5120
Tin Dizdarevic <tinovmail@gmail.com>
2022-03-09 19:32:31 +0000

    Add api url to environment variable.

--- src/app/modules/modules-routing.module.ts
+++ src/app/modules/modules-routing.module.ts
@@ -2,14 +2,14 @@ import {RouterModule, Routes} from "@angular/router";
 import {ModulesComponent} from "./modules.component";
 import {NgModule} from "@angular/core";
 import {BaseResolver} from "../interaction/base-resolver";
-
+import {environment} from "../../environments/environment";
 
 const routes: Routes = [
     {
         path: '',
         component: ModulesComponent,
         resolve: { filterData: BaseResolver },
-        data: { url: 'http://localhost:3000/modulesViewModel' }
+        data: { url: environment.baseUrl + environment.modulesUrl }
     }
 ]
 
