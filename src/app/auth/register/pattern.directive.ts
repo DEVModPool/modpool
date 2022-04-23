69e843d4788ef447bad42fc633cfe8e9fb1d20e5
Tin Dizdarevic <tinovmail@gmail.com>
2022-04-23 19:56:32 +0200

    Finalize sign up validation.

---
+++ src/app/auth/register/pattern.directive.ts
@@ -0,0 +1,16 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
+
+export function patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        if (!control.value) {
+            // if control is empty return no error
+            return null;
+        }
+
+        // test the value of the control against the regexp supplied
+        const valid = regex.test(control.value);
+
+        // if true, return no error (no error), else return error passed in the second parameter
+        return valid ? null : error;
+    };
+}
